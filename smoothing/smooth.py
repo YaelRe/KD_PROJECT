dda0fd1d30581968807c707f739034eba9471d0b
YaelRe <yaelreches3@gmail.com>
2021-12-23 16:09:41 +0200

    numpy instead of cpu()

--- smoothing/smooth.py
+++ smoothing/smooth.py
@@ -52,7 +52,7 @@ class Smooth:
 
         if mode is not None:
             stacked_outputs = torch.stack(outputs)
-            stacked_outputs = stacked_outputs.detach().cpu()
+            stacked_outputs = stacked_outputs.detach().numpy()
             df = pd.DataFrame(
                 stacked_outputs.reshape([stacked_outputs.shape[0] * stacked_outputs.shape[1], stacked_outputs.shape[2]]))
             df['batch_number'] = batch_idx
