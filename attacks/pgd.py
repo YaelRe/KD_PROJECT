c94705a2fe27cd7d5be26cb4d76f68e096cd05ea
adinak <adinak@campus.technion.ac.il>
2022-03-08 13:46:18 +0200

    added print

--- attacks/pgd.py
+++ attacks/pgd.py
@@ -57,7 +57,7 @@ class PGD(Attack):
                 curr_iter = rest * (self.n_iter + 1) + k
                 pert.requires_grad_()
                 x_i = x + pert
-                print(f"param device: {self.model.parameters.device}")
+                print(f"model device: {self.model.device}")
                 oi = self.model.forward(x_i)
                 probs = torch.softmax(oi, dim=1)
                 succ = torch.argmax(oi, dim=1) != y
